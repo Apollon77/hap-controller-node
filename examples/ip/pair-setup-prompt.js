72b87a4dc40a689bed6862f028e93ae1d437fac8
James Willcox <snorp@snorp.net>
2020-01-16 17:18:15 -0600

    Add startPairing() and finishPairing() to clients.
    This allows you to start the pairing process without a pin, which
    is required for some devices like the Ecobee 3. Starting pairing
    causes it to show the randomly-generated pin, which can then be
    supplied to finishPairing() to complete the process.

---
+++ examples/ip/pair-setup-prompt.js
@@ -0,0 +1,22 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const {HttpClient, IPDiscovery} = require('../../lib');
+
+const discovery = new IPDiscovery();
+
+discovery.on('serviceUp', async (service) => {
+  console.log('Found device!');
+
+  const client = new HttpClient(service.id, service.address, service.port);
+
+  const data = await client.startPairing();
+  readline.question('Enter PIN: ', async (pin) => {
+    await client.finishPairing(data, pin);
+    console.log('Paired! Keep the following pairing data safe:');
+    console.log(JSON.stringify(client.getLongTermData(), null, 2));
+  });
+});
+discovery.start();
