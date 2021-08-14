9053965bb057f44f9b956b7301960a10708547b9
Ingo Fischer <github@fischer-ka.de>
2021-08-15 01:50:32 +0200

    Update examples/ip/pair-setup.js
    Co-authored-by: Michael Stegeman <michael@stegeman.me>

--- examples/ip/pair-setup.js
+++ examples/ip/pair-setup.js
@@ -14,6 +14,6 @@ discovery.on('serviceUp', (service) => {
       console.log(`${service.name} paired! Keep the following pairing data safe:`);
       console.log(JSON.stringify(client.getLongTermData(), null, 2));
     })
-    .catch((e) => console.error(service.name, ' ', e));
+    .catch((e) => console.error(`${service.name}:`, e));
 });
 discovery.start();
